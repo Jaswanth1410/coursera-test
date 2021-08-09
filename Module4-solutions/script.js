a7a4d7105981caa737a0a433cbcef142786f13b0
Jaswanth1410 <jaswanthsaikakavakam@gmail.com>
2021-08-09 15:24:18 +0530

    commiting Module-4

--- Module4-solutions/script.js
+++ Module4-solutions/script.js
@@ -39,9 +39,9 @@ WARNING!!! WARNING!!!
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   
   for (var i = 0; i < names.length; i++) {
-    var firstLetter = names[i].charAt(0).toLowerCase();
+    var firstLetter = names[i].charAt(0);
   
-    if (firstLetter === 'j') {
+    if (firstLetter === 'j' || firstLetter === 'J') {
       byeSpeaker(names[i]);
     }
     else {
